07ebb9980c501d25c0e9a41199990ccceed2b2af
WalterIt <wshiruba@gmail.com>
2024-09-21 12:36:31 -0400

    ADDED: ClerkProvider - Theme; Sign-In & Sign-Up. INSTALLED: Uploadthing IMPLEMENTED: Globals.css

---
+++ src/app/site/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+      <main className="flex flex-col gap-8  items-center sm:items-start">
+        <h1 className="text-5xl font-bold">Welcome to Next.js!</h1>
+      </main>
+      
+  );
+}
