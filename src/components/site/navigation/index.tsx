6f009ea768c60874b888e3fdf71df5ebf50ccb8f
WalterIt <wshiruba@gmail.com>
2024-09-25 11:20:06 -0400

    INSTALLED: Prisma & Prisma/Client.  IMPLEMENTED: Routes in the Middleware.ts

--- src/components/site/navigation/index.tsx
+++ src/components/site/navigation/index.tsx
@@ -12,12 +12,14 @@ type Props = {
 const Navigation = ({user}: Props) => {
   return (
     <div className={"p-4 flex items-center justify-between relative"}>
+      <Link href={"/"}>
       <aside className={"flex items-center gap-2"}>
         <Image src={"./assets/plura-logo.svg"} alt={"Site LOGO"} width={40} height={40}/>
         <span className={"text-xl font-bold"}>
         YourSite
         </span>
       </aside>
+      </Link>
       <nav className={"hidden md:block absolute left-[50%] transform translate-x-[-50%] translate-y-[50%] "}>
         <ul className={"flex items-center justify-center gap-8 mt-[-10px] font-semibold "}>
           <Link href={"#"}>Pricing</Link>
